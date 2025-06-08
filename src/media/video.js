af0ac22646553624dfc4b193b098dd717e7f6df0
LeonSchampach <92578089+LeonSchampach@users.noreply.github.com>
2025-06-08 13:23:50 +0200

    feat: embed video with custom controls

---
+++ src/media/video.js
@@ -0,0 +1,73 @@
+const video = document.getElementById("video");
+const playPauseBtn = document.getElementById("playPause");
+const muteBtn = document.getElementById("muteBtn");
+const seekBar = document.getElementById("seekBar");
+const volumeSlider = document.getElementById("volume");
+
+
+function playPause() {
+    video.paused ? video.play() : video.pause();
+}
+// function fullscreen() {
+//     if (!document.fullscreenElement) {
+//         video.requestFullscreen();
+//     } else {
+//         document.exitFullscreen();
+//     }
+// }
+
+function forwards() {
+    video.currentTime += 5;
+}
+function backwards() {
+    video.currentTime -= 5;
+}
+
+muteBtn.addEventListener('click', () => {
+    video.muted = !video.muted;
+    if (video.muted) {
+        muteBtn.innerHTML = "<i class='fas fa-volume-mute text-xl'></i>"
+    }
+    else {
+        adjustVolumeIcon();
+    }
+});
+
+video.addEventListener('click', playPause);
+
+video.addEventListener("play", () => {
+    playPauseBtn.innerHTML = "<i class='fas fa-circle-pause text-3xl'></i>";
+});
+
+video.addEventListener("pause", () => {
+    playPauseBtn.innerHTML = "<i class='fas fa-circle-play text-3xl'></i>";
+});
+
+video.addEventListener("timeupdate", () => {
+    seekBar.value = video.currentTime;
+});
+
+seekBar.addEventListener("input", () => {
+    video.currentTime = seekBar.value;
+});
+
+video.addEventListener("loadedmetadata", () => {
+    seekBar.max = video.duration;
+});
+
+volumeSlider.addEventListener("input", () => {
+    video.volume = volumeSlider.value;
+    adjustVolumeIcon();
+});
+
+function adjustVolumeIcon() {
+    if (volumeSlider.value > .5) {
+        muteBtn.innerHTML = "<i class='fas fa-volume-high text-xl'></i>";
+    }
+    else if (volumeSlider.value > 0) {
+        muteBtn.innerHTML = "<i class='fas fa-volume-low text-xl'></i>";
+    }
+    else {
+        muteBtn.innerHTML = "<i class='fas fa-volume-off text-xl'></i>";
+    }
+}
